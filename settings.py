98fbc3daa077c09d03ac9203241cb8255aa09ed7
programmingispain <rahayward@proton.me>
2024-06-06 23:14:25 +0200

    idk

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ SESSION_CONFIGS = [
          name='centipede_game',
          display_name='Centipede Game',
          app_sequence=['centipede_game'],
-         num_demo_participants=4,
+         num_demo_participants=2,
      ),
     dict(
         name='centipede_game_control',
@@ -29,7 +29,12 @@ SESSION_CONFIGS = [
         num_demo_participants=4,
     ),
 ]
-
+ROOMS = [
+    dict(
+        name='QMEE_EXPERIMENT',
+        display_name='Stone Cold Creamery',
+    )
+]
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
 # in SESSION_CONFIGS, except those that explicitly override it.
 # the session config can be accessed from methods in your apps as self.session.config,
